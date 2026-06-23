96ac279ddaa9a1d1b476dfa3b023cdc895e25b34
hectordevilleroche-source <hectordevilleroche@gmail.com>
2026-06-23 11:19:22 +0200

    create constant.php
    creation constant.php

---
+++ php/constant.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define('DB_HOST', '90.19.117.149');   // Adresse du serveur de base de données
+define('DB_NAME', 'hjodon28');    // Nom de la base de données
+define('DB_USER', 'hjodon28');        // Nom d'utilisateur
+define('DB_PASS', '06SX8W698XSu7e9D');            // Mot de passe
+define('DB_CHARSET', 'utf8mb4');
+?>
