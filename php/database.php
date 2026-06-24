7beff1536601233b6a6ff2fdfda0e5774201d93c
hectordevilleroche-source <hectordevilleroche@gmail.com>
2026-06-24 09:34:36 +0200

    create data base
    create data base

---
+++ php/database.php
@@ -0,0 +1,20 @@
+<?php
+require_once('constant.php');
+function dbConnect()
+{
+    try
+    {
+        $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8',
+            DB_USER, DB_PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $exception)
+    {
+        error_log('Connection error: '.$exception->getMessage());
+        return false;
+    }   
+    return $db;
+}
+
+
+?>
